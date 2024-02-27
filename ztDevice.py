ac4a5190b2a2c5dc3a502c810605b951cc515d41
Peter.Hsieh <peter.hsieh@ecloudvalley.com>
2024-02-27 10:42:06 +0800

    test

--- ztDevice.py
+++ ztDevice.py
@@ -299,19 +299,22 @@ def getSecurityPolicyName(profileName):
 
 def pushConfig(conn,configBody):
     output = None
-    stage_first_OK = None
-    stage_second_OK = None
+    stage_first_OK = False
+    stage_second_OK = False
     o = configurationManagement.configurationManagement(conn)
     pushJobs = o.paConfigPush(configBody)
     if not (pushJobs["code"] in PRISMA_ACCESS_RESP_OK):
         print("Failed to push config")
         exit()
     job_id = int(pushJobs["resp"]["job_id"])
-    print("Push stage 1 --")
     while (True):
-        jobs = o.paConfigJobsListById(str(job_id),MOBILE_USERS)
-        result_str = jobs["data"][0]["result_str"]
-        print(result_str)
+        print("Pushing configuration stage #1 - validating")
+        try:
+            jobs = o.paConfigJobsListById(str(job_id),MOBILE_USERS)
+            result_str = jobs["data"][0]["result_str"]
+        except KeyError:
+            continue
+            
         match result_str:
             case "PEND":
                 time.sleep(3)
@@ -322,11 +325,14 @@ def pushConfig(conn,configBody):
             case _:
                 break
     if stage_first_OK:
-        print("Push stage 2 --")
         while (True):
-            jobs = o.paConfigJobsListById(str(job_id),MOBILE_USERS)
-            result_str = jobs["data"][0]["result_str"]
-            print(result_str)
+            print("Pushing configuration stage #2 - deployment")
+            try:
+                jobs = o.paConfigJobsListById(str(job_id),MOBILE_USERS)
+                result_str = jobs["data"][0]["result_str"]
+            except KeyError:
+                continue
+            
             match result_str:
                 case "PEND":
                     time.sleep(3)
@@ -335,7 +341,11 @@ def pushConfig(conn,configBody):
                     break
                 case _:
                     break
-    output = stage_first_OK & stage_second_OK           
+    output = stage_first_OK & stage_second_OK
+    if output:
+        print("Pushing configuration - OK")
+    else:
+        print("Pushing configuration - FAIL")
     return output
 if __name__ == '__main__':
     conn = getPrismaAccessConn()
